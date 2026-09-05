63f8442c8ef432d91cc510f4f7427d7b81a86afe
PyroByte <alinafea4real@gmail.com>
2026-09-05 10:44:09 +0300

    Externalized N_Token from narcy.py into a modular 'Token' model.

--- modules/DatatypeModel.py
+++ modules/DatatypeModel.py
@@ -1,6 +1,13 @@
+from dataclasses import dataclass
+
 class Model:
     def __init__(self, identifier : str, start : str, end : str = '', double : bool = False):
         self.identifier = identifier
         self.start = start
         self.end = end
         self.double = double
+
+@dataclass
+class Token:
+    token: str
+    type: str

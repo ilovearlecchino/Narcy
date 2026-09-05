a33cb54ca9961a0799720061318f392ece70b4b5
PyroByte <alinafea4real@gmail.com>
2026-09-05 10:43:30 +0300

    Added 'BadToken' and 'UnregisteredKeyword'

--- modules/Errors.py
+++ modules/Errors.py
@@ -9,3 +9,13 @@ class NoKeywords(Exception):
 class UnexpectedCharacterDuringFetch(Exception):
     """Normally a regular user wouldnt get this unless they tampered with the models."""
     pass
+
+class BadToken(Exception):
+    """When the parser receives an unexpectd token. Usually due to a bug in the code."""
+    pass
+
+class UnregisteredKeyword(Exception):
+    """Regular users will virtually never experience this error.
+    However, if one attempts to add new keyword methods without registering them-
+    then this error will be thrown."""
+    pass

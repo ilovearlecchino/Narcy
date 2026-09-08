f9b088efbd42fc55269d2d16bcd66533dedca568
PyroByte <alinafea4real@gmail.com>
2026-09-08 22:52:23 +0300

    implemented a chunker for expressions (pre-pass marco-parser)

--- core/parser.py
+++ core/parser.py
@@ -1,7 +1,9 @@
+from modules.DatatypeModel import Token
 from modules.ParseModels import *
 from modules.Errors import BadToken
 from modules.Errors import UnregisteredKeyword
 from modules.KeywordRegistry import LoadRegistry
+from pprint import pprint
 
 _KEYWORD_REGISTRY = LoadRegistry()
 _BLOCKED = {"else"}
@@ -16,32 +18,76 @@ _OPERATOR_BP = {
 _EXPRESSION_ALLOWED_TYPES = {
     "string",
     "numeric",
-    "operator"
+    "operator",
+    "statement"
 }
 
-def infix_operator_bp(operator:object) -> tuple:
-    if operator.type == 'operator':
+def Eof_Safe(func):
+    def wrapper(self, *args, **kwargs):
         try:
-            return _OPERATOR_BP[operator.token]
-        except KeyError:
-            raise BadToken(f"""Bad Token: Operator was either improperly pushed as a token
-            or unregistered.""")
-    else:
-        raise BadToken(f"""Unexpected Token: Expected operator, got f{operator.type} instead.""")
+            return func(self, *args, **kwargs)
+        except IndexError:
+            return Token('eof', 'eof')
+    return wrapper
+    
+class BaseParser:
+    def __init__(self, tokens):
+        self.index = 0
+        self.tokens = tokens
+
+    @Eof_Safe
+    def peek_next(self):
+        return self.tokens[self.index + 1]
+
+    @Eof_Safe
+    def peek(self):
+        return self.tokens[self.index]
+
+    @Eof_Safe
+    def advance(self):
+        tok = self.tokens[self.index]
+        self.index += 1
+        return tok
+
+class ExpressionAnalyzer(BaseParser):
+    def convert_into_expressions(self):
+        _TOKS = []
+        _EXPR = []
+        _TOK = self.advance()
+
+        while _TOK.type != 'eof':
+            if _TOK.type in _EXPRESSION_ALLOWED_TYPES:
+                if self.peek().type in _EXPRESSION_ALLOWED_TYPES:
+                    _EXPR.append(_TOK)
+                else:
+                    if not _EXPR:
+                        # Single isolated token, append directly
+                        _TOKS.append(_TOK)
+                    else:
+                        # Append final token in sequence and create expression node
+                        _EXPR.append(_TOK)
+                        _TOKS.append(ExpressionNode(_EXPR))
+                        _EXPR = []
+            else:
+                _TOKS.append(_TOK)
+
+            _TOK = self.advance()
+
+        _TOKS.append(Token('eof', 'eof'))
+        return _TOKS
+                    
+
+def _INFIX_BP(operator : Token):
+    try:
+        return _OPERATOR_BP[operator.token]
+    except KeyError:
+        raise BadToken(f"""Unrecognized operator: {operator.token}""")
+
+    
 
 def module_execute(args):
     CurrentNode = None
-    Expression = [False, []]
-
-    for index, item in enumerate(args):
-        if args[index].type == 'keyword':
-            try:
-                CurrentNode = _KEYWORD_REGISTRY[args[index].token]
-            except KeyError:
-                if args[index].token not in _BLOCKED:
-                    raise UnregisteredKeyword(
-                                        f"""Keyword '{args[index].token}' not found in registry."""
-                                        )
-        # Come back here to finish reading
-            
+    args.append(Token('eof', 'eof'))
+    args = ExpressionAnalyzer(args).convert_into_expressions()
+    pprint(args)
     return args

598c252a1ae2bfb721e429a5e629ba8ed739e21f
PyroByte <alinafea4real@gmail.com>
2026-09-14 18:20:16 +0300

    Added BadTree, UnregisteredTreeModel, and InvalidSyntax

--- modules/Errors.py
+++ modules/Errors.py
@@ -14,8 +14,19 @@ class BadToken(Exception):
     """When the parser receives an unexpectd token. Usually due to a bug in the code."""
     pass
 
+class BadTree(Exception):
+    """When a badly configured tree is passed into the parser"""
+
 class UnregisteredKeyword(Exception):
     """Regular users will virtually never experience this error.
     However, if one attempts to add new keyword methods without registering them-
     then this error will be thrown."""
+    pass
+
+class UnregisteredTreeModel(Exception):
+    """Similar to the above. An unregistered tree configuration was accessed."""
+    pass
+
+class InvalidSyntax(Exception):
+    """Raised due to bad code"""
     pass

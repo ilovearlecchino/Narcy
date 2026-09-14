f5ccc1a03192f1d9b532a28ea325dcd779030d69
PyroByte <alinafea4real@gmail.com>
2026-09-14 18:21:04 +0300

    Added ConditionNode

--- modules/ParseModels.py
+++ modules/ParseModels.py
@@ -15,6 +15,7 @@ class ValueHolderNode:
     def __post_init__(self):
         """I noticed that the chunker returns a list of items due to the way it is designed
         this function ensures that the value is not a list as it's not meant to hold a list.
+        This may be copy pasted into other functions
         """
         if isinstance(self.value, list) and len(self.value) == 1:
             self.value = self.value[0]
@@ -24,21 +25,34 @@ class BinaryOperationNode:
     operation : str
     left : object
     right : object
+    type : str = "binaryoperation"
+
 
 @dataclass
 class AssignNode:
     name : str
     value : object
+    type : str = "assignment"
 
 @dataclass
 class OutputNode:
     value : object
+    type : str = "output"
 
 @dataclass
 class IfNode:
-    condition : object
+    condition : list
     body : list
     else_body: Optional[List] = None
+    side_conditions: Optional[List] = None
+
+    type : str = "if"
+
+@dataclass
+class ConditionNode:
+    condition : list
+    body: list
+    type : str = "branchedcondition"
 
 Nodes = {
     ExpressionNode,
@@ -46,7 +60,8 @@ Nodes = {
     AssignNode,
     OutputNode,
     ValueHolderNode,
-    IfNode
+    IfNode,
+    ConditionNode
 }
 
 def NodesRepr() -> dict:

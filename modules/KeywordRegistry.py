70f1fe3fe571aa5659ad174b331a68fc5cc79e9d
PyroByte <alinafea4real@gmail.com>
2026-09-14 18:20:38 +0300

    Added two more functions

--- modules/KeywordRegistry.py
+++ modules/KeywordRegistry.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from modules.ParseModels import Nodes, NodesRepr
+from modules.Errors import UnregisteredTreeModel
 import json
 
 @dataclass
@@ -15,3 +16,23 @@ def LoadRegistry() -> dict:
         for keyword in UnassignedMap:
             _REGISTRY[keyword] = MAP[UnassignedMap[keyword]]
     return _REGISTRY
+
+def GetStaticTypes():
+    with open('registry.json', 'r') as f:
+        _RET = json.loads(f.read())['StaticTypes']
+    return _RET
+
+def GetTreeConfig(keyword):
+    with open('treemodels.json', 'r') as f:
+        _MODS = json.loads(f.read())['TreeModels']
+        for model in _MODS:
+            if model['ID'] == keyword:
+                return model['InputConfig']
+        return UnregisteredTreeModel(
+            f"""Unregistered tree configuration: {keyword}""",
+            """If you've added a new tree model make sure to register it in treemodels.json"""
+        )
+
+def GetTDRDPMatchLookup():
+    with open('registry.json', 'r') as f:
+        return json.loads(f.read())['TDRDPMatchLookup']
